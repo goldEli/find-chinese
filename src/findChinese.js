ba8a68a991bcff0d2853157c5413984e29a9d2b5
miaoyu <miaoyu@ifun.com>
2023-03-17 16:46:08 +0800

    feat: get all files in dir

--- src/findChinese.js
+++ src/findChinese.js
@@ -1,6 +1,21 @@
+import chalk from "chalk";
+import { getAllFilesByDir, readFile } from "./utils";
 
 const findChinese = (options) => {
-    console.log('findChinese')
+  const rootDir = process.cwd();
+  console.log(chalk.gray(`脚本运行的路径:${rootDir}`));
+
+  getAllFilesByDir(
+    {
+      dirPath: rootDir,
+      fileExtension: ["js", "vue", "ts", "tsx"],
+    },
+    async(filePath) => {
+      console.log(filePath);
+      const code = await readFile(filePath)
+      
+    }
+  );
 };
 
 export default findChinese;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,7 @@
 import chalk from "chalk";
 import ora from "ora";
+import fs from "fs";
+import path from "path";
 
 export const log = {
   warn: (msg) => {
@@ -17,3 +19,74 @@ export const createLoading = (msg) => {
   const spinner = ora(msg).start();
   return spinner;
 };
+
+/**
+ * 文件遍历方法
+ * @param dirPath 需要遍历的文件路径
+ */
+export async function getAllFilesByDir(options, callback) {
+  const { dirPath, fileExtension } = options;
+  fs.readdirSync(dirPath).forEach((fileName) => {
+    const filePath = path.join(dirPath, fileName);
+    fs.stat(filePath, function (error, stats) {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      const isFile = stats.isFile();
+      const isDir = stats.isDirectory();
+      if (isFile) {
+        if (fileExtension.some((item) => filePath.endsWith(item))) {
+          callback(filePath);
+        }
+      }
+      if (isDir) {
+        getAllFilesByDir(
+          {
+            dirPath: filePath,
+            fileExtension,
+          },
+          callback
+        );
+      }
+    });
+  });
+}
+
+async function checkCodeHasChinese(code, filename) {
+  const ast = codeToAst(code);
+  const res = await handleAst(ast);
+  if (res) {
+    // console.log(JSON.stringify(ast));
+    console.log("此文件存在中文", filename);
+  }
+}
+
+export function readFile(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      resolve(data);
+    });
+  });
+}
+
+export function getFileType(fileName) {
+  if (fileName.endsWith(".ts")) {
+    return "ts";
+  }
+  if (fileName.endsWith(".vue")) {
+    return "vue";
+  }
+  if (fileName.endsWith(".js")) {
+    return "js";
+  }
+  if (fileName.endsWith(".tsx")) {
+    return "tsx";
+  }
+  console.log(chalk.red(`${fileName},文件类型不支持`));
+  return "unknown";
+}
