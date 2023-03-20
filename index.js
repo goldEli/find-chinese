408266f25f7282bd3563288495b2e119c5620207
miaoyu <miaoyu@ifun.com>
2023-03-20 14:08:47 +0800

    feat: check version

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import findChinese from "./src/findChinese.js";
 import arg from "arg";
 import chalk from "chalk";
+import profile from "./package.json";
+import { log } from "./src/utils/index.js";
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
       "-h": Boolean,
       "--i": String,
+      "-v": Boolean,
     },
     {
       argv: rawArgs.slice(2),
@@ -18,6 +21,7 @@ function parseArgumentsIntoOptions(rawArgs) {
   return {
     ignoreDirsAndFiles: ignoreDirsAndFiles,
     help: args["-h"],
+    version: args["-v"],
     suffix: args["--suffix"],
     // command: args._[0],
   };
@@ -25,7 +29,11 @@ function parseArgumentsIntoOptions(rawArgs) {
 
 export async function run(args) {
   let options = parseArgumentsIntoOptions(args);
-  console.log(chalk.green("开始检测包含文件的中文..."));
+  if (options.version) {
+    log.suc(JSON.stringify(profile.version));
+
+    return;
+  }
   if (options.help) {
     console.log("Usage: find-chinese [options]");
     console.log();
@@ -36,12 +44,13 @@ export async function run(args) {
     // console.log("Commands:");
     // console.log("\tcreate\t创建渐进式taro项目");
     console.log(
-      "\t\t --i 忽略的文件夹, 默认过滤 node_modules 文件夹 中竖向 'dir|src'"
+      "\t\t --i 忽略的文件夹或者文件夹, 默认过滤 node_modules 文件夹 中竖向 'dir|src'"
     );
     console.log();
     // console.log("\twatch\t文件监听");
     // console.log("\tconfigWX\t创建/修改 微信开发配置");
     return;
   }
+  console.log(chalk.green("开始检测包含文件的中文..."));
   findChinese(options);
 }
